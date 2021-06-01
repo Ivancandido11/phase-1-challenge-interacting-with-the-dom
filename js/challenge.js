b526a6dcf20a682544182f58e525c2c1774b8666
Ivan Candido <ivan.candido@powerhrg.com>
2021-06-01 16:28:43 -0400

    Completed all challenges

--- js/challenge.js
+++ js/challenge.js
@@ -1,18 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const playing = !0
-    const timer = function() {
-        return setInterval(function (){
-            let counterElement = document.getElementById('counter')
-            const counterNum = parseInt(counterElement.innerText)
-            counterElement.innerText = counterNum + 1
-        }, 1e3)
+    function timer() {
+        let timerStart
+        return {
+            start() {
+                timerStart = setInterval(function (){
+                let counterElement = document.getElementById('counter')
+                const counterNum = parseInt(counterElement.innerText)
+                counterElement.innerText = counterNum + 1
+            }, 1e3)}, 
+            stop () {
+                clearInterval(timerStart)
+            }
+        }
     }
     const interval = timer()
+    interval.start()
+    let counter = document.getElementById('counter')
     const minus = document.getElementById("minus")
     const plus = document.getElementById("plus")
     const heart = document.getElementById("heart")
     const pause = document.getElementById("pause")
-    const commentForm = document.getElementsByTagName("form")[0]
+    const submit = document.getElementById('comment-form')
+
+
+    function submitComment(event){
+        event.preventDefault()
+        const commentForm = document.querySelector('#list')
+        const comment = document.querySelector('#comment-input')
+        const newP = document.createElement('p')
+        newP.innerText = comment.value
+        commentForm.append(newP)
+        comment.value = ""
+    }
+
+    pause.addEventListener("click", pauseTimer)
 
     minus.addEventListener("click", function(){
         let counterElement = document.getElementById('counter')
@@ -29,8 +50,6 @@ document.addEventListener("DOMContentLoaded", () => {
         let counterElement = document.getElementById('counter')
         const counterNum = parseInt(counterElement.innerText)
         const likes = document.querySelector('.likes')
-
-        console.log(secondsArray)
         if (secondsArray.includes(counterNum)){
             secondsArray.push(counterNum)
             const times = secondsArray.filter( (a) => a === counterNum)
@@ -45,4 +64,22 @@ document.addEventListener("DOMContentLoaded", () => {
             likes.append(likesElement)
         }
     })
+    function pauseTimer(){
+        if (pause.innerText === 'pause'){
+            interval.stop()
+            minus.disabled = true
+            plus.disabled = true
+            heart.disabled = true
+            submit.disabled = true
+            pause.innerText = 'resume'
+        } else {
+            interval.start()
+            minus.disabled = false
+            plus.disabled = false
+            heart.disabled = false
+            submit.disabled = false
+            pause.innerText = 'pause' 
+        }
+    }
+    submit.addEventListener("submit", submitComment)
 })
